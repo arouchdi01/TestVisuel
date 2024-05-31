c48b37513f1cbd96228a0ad94e001b213a6fafc6
arouchdi01 <ahamadarouchdi@gmail.com>
2024-05-31 15:50:20 +0000

    first commit

---
+++ cypress/e2e/test_Visuel.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+describe('Test Technique', () => {
+  beforeEach(()=>{
+    cy.visit('https://maasil.learnybox.com/page-de-test-maasil/');
+   });
+  
+   it('Vérification de la conformité visuelle par rapport à la maquette fournie', () => {
+    
+  })
+})
